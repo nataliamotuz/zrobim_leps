bc41b0df4da2127937ce6c44b3a7cef2e73f233e
nataliamotuz <natalia.motuz@gmail.com>
2020-04-03 12:04:14 +0300

    feat: change text

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $('.countdown').countdown('2020/04/19 23:59:59', function (event) {
+  $('.countdown').countdown('2020/04/26 23:59:59', function (event) {
     $('.day').html(event.strftime('%D'));
     $('.hour').html(event.strftime('%H'));
     $('.minute').html(event.strftime('%M'));
